a36cf32c2d6a31650a43968e9f636af9c7463c68
brunobarreira <brunobarreirasilva@gmail.com>
2021-09-30 11:26:25 -0300

    refactor: use todo repository from @todo/integration_features module

--- command-line-app/src/index.cli.ts
+++ command-line-app/src/index.cli.ts
@@ -1,10 +1,25 @@
 import { Todo } from '@todoapp/business'
+import { AddTodoRepository } from '@todoapp/integration_features'
 
-console.log("[***************************************]")
-console.log("[WELCOME TO THE SIMPLE TODO APP MONOREPO]")
-console.log("[***************************************]")
+async function main() {
 
-const newTodo: Todo = {
-    title: 'any_todo_awesome_title',
-    description: 'any_todo_awesome_description'
+    console.log("[***************************************]")
+    console.log("[WELCOME TO THE SIMPLE TODO APP MONOREPO]")
+    console.log("[***************************************]")
+
+    const newTodo: Todo = {
+        title: 'any_todo_awesome_title',
+        description: 'any_todo_awesome_description'
+    }
+
+    const repository: AddTodoRepository = new AddTodoRepository()
+
+    const add = await repository.addTodo(newTodo)
+
+    console.log(add)
+
+    console.log("[DONE]")
 }
+
+main()
+
