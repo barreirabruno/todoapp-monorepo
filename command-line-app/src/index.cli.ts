5e9496cb9733921cfad26f100e018911e13ba398
brunobarreira <brunobarreirasilva@gmail.com>
2021-09-30 10:48:36 -0300

    refactor: remove unsued module features of cli index

--- command-line-app/src/index.cli.ts
+++ command-line-app/src/index.cli.ts
@@ -1,5 +1,4 @@
 import { Todo } from '@todoapp/business'
-import { AddTodoRepository } from '@todoapp/frameworks'
 
 console.log("[***************************************]")
 console.log("[WELCOME TO THE SIMPLE TODO APP MONOREPO]")
@@ -9,12 +8,3 @@ const newTodo: Todo = {
     title: 'any_todo_awesome_title',
     description: 'any_todo_awesome_description'
 }
-
-const repository:AddTodoRepository = new AddTodoRepository();
-
-async function addTodoInRepo() {
-    return await repository.addTodo(newTodo);
-}
-
-console.log("[ADD TODO]:", addTodoInRepo())
-console.log("[TODO ADDED]")
